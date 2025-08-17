dc0783567b239fa43851c5beae9bfb9deb1e34f4
crontreras1 <crontreras1@gmail.com>
2025-08-17 14:03:09 -0500

    class 5. automatic validation with express and openapi validator

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
+const OpenApiValidator = require('express-openapi-validator'); 
 const YAML = require('yamljs');
+
 const app = express();
 const port = 3000;
 
-const swaggerDocuments = YAML.load('./openapi.yaml');
+const swaggerDocument = YAML.load('./openapi.yaml');
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocuments));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: swaggerDocument,
+    validateRequests: true,
+    validateResponses: true,
+    ignorePaths: /.*\/docs.*/,
+  })
+);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors
+  });
+});
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Este es el home' });
+});
 
 app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello world'});
+  res.json({ message: 'Holi, mundillo ;)' });
 });
 
 app.listen(port, () => {
